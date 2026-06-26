f98a27182a09a63197126367814e41e103c8dc46
Lazar Popovic <lazarpopovicgaming@gmail.com>
2026-06-26 19:35:52 +0200

    validation added for input field

--- src/js/detonateButton.js
+++ src/js/detonateButton.js
@@ -5,9 +5,11 @@ import { surfaceView } from "./view/surfaceView";
 import { marker } from "./script";
 import { map } from "./script";
 import { buildEffectCircles, displayCircles } from "./helpers/displayCircle";
+import { validateYieldInput } from "./helpers/validateYield";
 import { zoomToEffect } from "./helpers/zoomLevel";
 
 export const displayEffects = document.querySelector(".effects__container");
+export const alertBox = document.querySelector(".alert");
 const detonateButton = document.querySelector(".main__button");
 const radioButtonAir = document.querySelector(".air");
 const radioButtonSurface = document.querySelector(".surface");
@@ -15,6 +17,11 @@ const kilotonsInput = document.querySelector(".warhead__input");
 
 function detonateButtonAction() {
   detonateButton.addEventListener("click", (e) => {
+    if (!validateYieldInput(kilotonsInput, alertBox)) {
+      e.preventDefault();
+      return;
+    }
+
     displayEffects.innerHTML = "";
     const center = [marker.getLatLng().lat, marker.getLatLng().lng];
     if (radioButtonAir.checked) {
